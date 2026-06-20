97b607003e4cfdda1756c9fbe4e53bdbbc088b77
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-06-20 10:32:55 +0600

    create post and get api

--- index.js
+++ index.js
@@ -31,10 +31,17 @@ async function run() {
 
         const database = client.db('hireloop_db');
         const JobsCollection = database.collection('jobs');
+        const companiesCollection = database.collection('companies')
 
         app.post('/api/jobs', async (req, res) => {
             const data = req.body
             const result = await JobsCollection.insertOne(data)
+            res.send(result)
+        })
+        app.post('/api/companies', async (req, res) => {
+            const data = req.body
+            console.log(data);
+            const result = await companiesCollection.insertOne(data)
             res.send(result)
         })
         app.get('/api/jobs', async (req, res) => {
@@ -48,6 +55,18 @@ async function run() {
             const result = await JobsCollection.find(query).toArray()
             res.send(result)
         })
+        app.get('/api/companies', async (req, res) => {
+            try {
+                const companies = await companiesCollection.find().toArray();
+                res.status(200).send(companies);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send({
+                    success: false,
+                    message: 'Failed to fetch companies'
+                });
+            }
+        });
 
 
         await client.db("admin").command({ ping: 1 });
